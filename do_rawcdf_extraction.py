29db146f513e54c2cbad1cf9031ca7afbf15b8ee
Benjamin Roberts <ben_roberts@terpalum.umd.edu>
2022-01-07 10:21:47 -0800

    Switch to ArgumentParser, add caching

--- do_rawcdf_extraction.py
+++ do_rawcdf_extraction.py
@@ -2,7 +2,9 @@
 
 import time
 import os
-from optparse import OptionParser
+import tempfile
+import shutil
+from argparse import ArgumentParser, Namespace
 from netCDF4 import Dataset, MFDataset
 import geopandas as gpd
 import numpy as np
@@ -36,38 +38,65 @@ def chunks(lst, n):
 
 def main():
     script_home = os.path.dirname(os.path.realpath(__file__))
-    parser = OptionParser(usage="%prog [options] incdf1... outcdf outvar")
-    parser.add_option("-d", dest="domain_node_shapefile",
+    parser = ArgumentParser(description="Extract data from SSM netcdf output files")
+    parser.add_argument("incdf", nargs="+", help="each input CDF file")
+    parser.add_argument("outcdf", help="the output CDF file (created if it doesn't exist)")
+    parser.add_argument("outvar", help="the variable to store extracted data in the output CDF")
+    parser.add_argument("-d", dest="domain_node_shapefile",
             help="Specify a domain node shapefile")
-    parser.add_option("--invar", dest="input_var",
+    parser.add_argument("--invar", dest="input_var",
             help="Extract the values of a different output variable")
-    parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
+    parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
             help="Print progress messages during the extraction")
-    parser.add_option("-c", "--chunk-size", type="int", dest="chunk_size",
+    parser.add_argument("-c", "--chunk-size", type=int, dest="chunk_size",
             help="Process this many CDF files at once")
+    parser.add_argument("--cache", dest="cache", action="store_true",
+            help="Use a read/write cache in a temporary directory")
     parser.set_defaults(chunk_size=4,
             domain_node_shapefile=os.path.join(script_home, domain_nodes_shp),
             input_var="DOXG",
             verbose=False)
-    (options, args) = parser.parse_args()
+    args = parser.parse_args()
 
-    exist_cdfs = args[:-2]
-    output_cdf = args[-2]
-    output_var = args[-1]
+    if args.cache:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            exist_cdfs = []
+            if args.verbose:
+                print("Caching input files...", flush=True)
+            for infile in args.incdf:
+                newpath = os.path.join(tmpdir, os.path.basename(infile))
+                shutil.copy(infile, newpath)
+                exist_cdfs.append(newpath)
+            output_cdf = os.path.join(tmpdir, os.path.basename(args.outcdf))
+            if os.path.exists(args.outcdf):
+                if args.verbose:
+                    print("Caching output file...", flush=True)
+                shutil.copy(args.outcdf, output_cdf)
+            do_extract(exist_cdfs, output_cdf, **vars(args))
+            # Copy the resulting output CDF back
+            if args.verbose:
+                print("Saving output file...", flush=True)
+            shutil.copy(output_cdf, args.outcdf)
+            if args.verbose:
+                print("Finished.", flush=True)
+    else:
+        do_extract(args.incdf, args.outcdf, **vars(args))
 
-    if options.verbose:
+def do_extract(exist_cdfs, output_cdf, **kwargs):
+    args = Namespace(**kwargs)
+    if args.verbose:
         print("Determining scope of work...", flush=True)
     indata = MFDataset(exist_cdfs)
-    node_ids = get_node_ids(options.domain_node_shapefile)
+    node_ids = get_node_ids(args.domain_node_shapefile)
     times_ct = len(indata.dimensions['time'])
-    if options.verbose:
+    if args.verbose:
         print("Initializing output file...", flush=True)
     if not os.path.exists(output_cdf):
         outdata = init_output(output_cdf, times_ct, node_ids)
         outdata['time'][:] = indata['time'][:] / 3600 / 24
     else:
         outdata = append_output(output_cdf)
-    init_output_var(outdata, output_var)
+    init_output_var(outdata, args.outvar)
 
     # Attempts to use the entire MFDataset don't seem to scale well.
     # Instead, I'm resorting to a blocking approach where MFDatasets are
@@ -75,24 +104,25 @@ def main():
     indata.close()
     i = 0
     total = 0
-    if options.verbose:
+    if args.verbose:
         print("Beginning extraction...", flush=True)
     start_time = time.perf_counter()
-    for cdfchunk in chunks(exist_cdfs, options.chunk_size):
+    for cdfchunk in chunks(exist_cdfs, args.chunk_size):
         c = MFDataset(cdfchunk)
         chunk_times = len(c.dimensions['time'])
-        data = c[options.input_var][:, -1, node_ids - 1]
-        outdata[output_var][i:i+chunk_times,:] = data
+        data = c[args.input_var][:, -1, node_ids - 1]
+        outdata[args.outvar][i:i+chunk_times,:] = data
         i += chunk_times
         c.close()
-        if options.verbose:
+        if args.verbose:
             elapsed = (time.perf_counter() - start_time)
             to_go = elapsed * (times_ct / i - 1)
             total += data.size * data.itemsize
             print("{0}/{1} ({2}s elapsed, {3}s to go, {4}KBps)".format(i,
-                times_ct, int(elapsed), int(to_go), int(total/elapsed/1000)), flush=True)
-    if options.verbose:
-        print("Finished.", flush=True)
+                times_ct, int(elapsed), int(to_go), int(total/elapsed/1000)),
+                flush=True)
+    if args.verbose:
+        print("Extraction finished.", flush=True)
     outdata.close()
 
 if __name__ == "__main__": main()
