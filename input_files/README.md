b213acebdabd7d7acdccc04c0af6400aab4314c8
Ben Roberts <ben@benroberts.me>
2023-02-28 11:19:16 -0800

    New freshwater input converters

--- input_files/README.md
+++ input_files/README.md
@@ -15,3 +15,67 @@ to easily make input files for various scenarios.
 Examples of these files are available from
 [SSMC](https://gitlab.com/ssmc/fvcom-icm4.0) and
 [WA Ecology](https://fortress.wa.gov/ecy/ezshare/EAP/SalishSea/SalishSeaModelBoundingScenarios.html).
+
+## Working with the `_riv.dat` and `_pnt_wq.dat` files
+
+I made most of these Python notebooks in 2022. In 2023, I created a new
+process that allows for unified, easier editing of the separate freshwater
+input files for the hydrodynamic and water quality models.
+
+The script `ssm_read_fwinputs.py` will create a multi-sheet Excel file of
+all the data from a hydrodynamic rivers file and the corresponding water
+quality file. Three worksheets will be created. The first will have the
+node list and any comments parsed from the original files, which generally
+say what feature the node corresponds to. A third column tracks whether or
+not the given source is present in the water quality input file (becuase
+not all of Ecology's loadings have water quality constituents). The second
+is the vertical distribution of inflow into the node. The third is all of
+the flow and water quality data for all nodes at all dates.
+
+```
+usage: ssm_read_fwinputs.py [-h] [-s START_DATE] [-v]
+                            riv_file pnt_wq_file outfile
+
+Convert freshwater dat files into Excel spreadsheet
+
+positional arguments:
+  riv_file              The FVCOM rivers file
+  pnt_wq_file           The FVCOM-ICM point source file
+  outfile               The destination spreadsheet file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s START_DATE, --start-date START_DATE
+                        The zero date for the file
+  -v, --verbose         Print progress messages during the conversion
+```
+
+The script `ssm_write_fwinputs.py` does the reverse: it takes the
+spreadsheet and converts it into a pair of input files for the models.
+
+```
+usage: ssm_write_fwinputs.py [-h] [-s START_DATE] [-v] [-c COMMENT]
+                             infile out_base
+
+Convert a point source discharge spreadsheet into SSM input files
+
+positional arguments:
+  infile                The point sources spreadsheet
+  out_base              The base of the output filename (_riv.dat and
+                        _wq.dat will be appended
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s START_DATE, --start-date START_DATE
+                        The zero date for the file. Defaults to the earliest
+                        date in the file
+  -v, --verbose         Print progress messages during the conversion
+  -c COMMENT, --comment COMMENT
+                        An optional comment to include in the first line
+```
+
+There are two sets of tests for these scripts.
+`test_ssm_read_fwinputs.py` is just a unit test for key parts of
+`ssm_read_fwinputs.py`. `riv_pnt_integration_test.ipynb` is a more complete
+integration test of the combined read and write operations to ensure the
+data that was read in is written back out again.
---
+++ input_files/ssm_read_fwinputs.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+"""Convert SSM freshwater input files into a discharge spreadsheet
+
+Script that converts a FVCOM rivers file paired with a FVCOM-ICM point
+sources file into an Excel spreadsheet that can be easily edited.
+"""
+
+from argparse import ArgumentParser, FileType
+import logging
+import sys
+from io import StringIO
+
+import numpy as np
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+# State variables in the order they are present in the file
+ALL_STATEVARS = ('discharge', 'temp', 'salt', 'tss',  'alg1', 'alg2', 'alg3', 'zoo1',
+                              'zoo2', 'ldoc', 'rdoc', 'lpoc', 'rpoc', 'nh4',  'no32',
+                              'urea', 'ldon', 'rdon', 'lpon', 'rpon', 'po4',  'ldop',
+                              'rdop', 'lpop', 'rpop', 'pip',  'cod',  'doxg', 'psi',
+                              'dsi',  'alg1p','alg2p','alg3p','dic',  'talk')
+
+def read_dat_file(f, start_date, num_statevars=None):
+    statevars = ALL_STATEVARS if num_statevars is None else ALL_STATEVARS[:num_statevars]
+    # See the FVCOM manual
+    inflow_type, point_st_type = np.loadtxt([next(f)], dtype=str,
+            comments='!')
+    # The total number of discharge nodes
+    num_qs = int(next(f))
+    # All the node numbers with discharges
+    # FIXME this won't work if there is no comment after the node number
+    nodes = []
+    comments = []
+    for l in range(num_qs):
+        line = next(f).split('!', maxsplit=1)
+        nodes.append(int(line[0].strip()))
+        comments.append(line[1].strip() if len(line) > 1 else np.nan)
+    node_df = pd.DataFrame({'Node': nodes, 'Comment': comments}).set_index('Node')
+    nodes = np.array(nodes)
+    # Depth distribution fractions into each node. Skipping the first
+    # (node count) column
+    vqdist = np.loadtxt([next(f) for l in range(num_qs)])[:,1:]
+
+    num_times = int(next(f))
+
+    # Initialize storage arrays
+    times = np.zeros(num_times)
+    statedata = {}
+    for v in statevars:
+        statedata[v] = np.zeros((num_times, num_qs))
+
+    for t in range(num_times):
+        times[t] = float(next(f))
+        for v in statevars:
+            statedata[v][t,:] = np.loadtxt([next(f)])
+
+    vqdist_df = pd.DataFrame(vqdist, index=node_df.index,
+            columns=np.arange(vqdist.shape[1])+1)
+
+    dates = pd.Timestamp(start_date) + pd.to_timedelta(times, 'h')
+    dates.name = 'Date'
+    node_data = []
+    for i,n in enumerate(nodes):
+        n_fill = np.zeros(len(times), dtype=np.int64) + n
+        df = pd.DataFrame({v: statedata[v][:,i] for v in statevars},
+                index=[dates,pd.Index(n_fill,name='Node')])
+        node_data.append(df)
+    node_data_df = pd.concat(node_data)
+
+    return ({
+        'nodes': node_df,
+        'vqdist': vqdist_df,
+        'data': node_data_df
+        }, inflow_type, point_st_type)
+
+def dfs_match(df1, df2):
+    """True if common indices/columns match, otherwise returns offending column(s)"""
+    mismatches = []
+    m = df1.join(df2, how='inner', rsuffix='_2')
+    for c in df1.columns.intersection(df2.columns):
+        if np.any(m[str(c)] != m[str(c)+'_2']):
+            mismatches.append(c)
+    return True if len(mismatches) == 0 else mismatches
+
+def read_merge_dats(riv_file, pnt_file, start_date):
+    """Read riv and pnt_wq dat files then return a merged copy of all data"""
+    dfs, inflow_type, point_st_type = read_dat_file(riv_file, start_date, num_statevars=3)
+    pnt_dfs, *junk = read_dat_file(pnt_file, start_date)
+
+    # Form a union of the node DFs
+    pnt_dfs['nodes']['ICM Source'] = True
+    all_nodes_df = dfs['nodes'].join(pnt_dfs['nodes'], how='outer',
+            rsuffix='_pnt')
+    all_nodes_df['Comment'].fillna(all_nodes_df['Comment_pnt'], inplace=True)
+    del all_nodes_df['Comment_pnt']
+    all_nodes_df['ICM Source'].fillna(False, inplace=True)
+
+    # Check that the VQDIST and data DFs have the same values for matching
+    # nodes
+    mismatches = dfs_match(dfs['vqdist'], pnt_dfs['vqdist'])
+    if mismatches is not True:
+        raise ValueError(f'vqdist columns {mismatches} do not match')
+    mismatches = dfs_match(dfs['data'], pnt_dfs['data'])
+    if mismatches is not True:
+        raise ValueError(f'nqtime columns {mismatches} do not match')
+
+    # Merge the VQDist DataFrames
+    all_vqdist_df = pd.concat((dfs['vqdist'], pnt_dfs['vqdist']))
+    # https://stackoverflow.com/a/34297689
+    all_vqdist_df = all_vqdist_df[~all_vqdist_df.index.duplicated(keep='first')]
+    # Merge the data DataFrames
+    cond = dfs['data'].index.isin(pnt_dfs['data'].index)
+    pruned_riv_data = dfs['data'].drop(dfs['data'][cond].index)
+    all_data_df = pd.concat((pruned_riv_data, pnt_dfs['data']))
+
+    return ({
+        'nodes': all_nodes_df,
+        'vqdist': all_vqdist_df,
+        'data': all_data_df
+        }, inflow_type, point_st_type)
+
+def write_spreadsheet(dfs, out_file_excel):
+    with pd.ExcelWriter(out_file_excel) as writer:
+        dfs['nodes'].to_excel(writer, sheet_name='Nodes')
+        dfs['vqdist'].to_excel(writer, sheet_name='VQDist')
+
+        dfs['data'].to_excel(writer, sheet_name='Data')
+
+def main():
+    parser = ArgumentParser(description="Convert freshwater dat files into Excel spreadsheet")
+    parser.add_argument("riv_file", type=FileType('r'),
+            help="The FVCOM rivers file")
+    parser.add_argument("pnt_wq_file", type=FileType('r'),
+            help="The FVCOM-ICM point source file")
+    parser.add_argument("outfile", type=FileType('wb'),
+            help="The destination spreadsheet file")
+    parser.add_argument("-s", "--start-date", type=pd.Timestamp,
+            default="2014.01.01", help="The zero date for the file")
+    parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
+            help="Print progress messages during the conversion")
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO if args.verbose else logging.WARNING)
+
+    logger.info("Reading files")
+    dfs, *junk = read_merge_dats(args.riv_file, args.pnt_wq_file, args.start_date)
+
+    logger.info("Writing the spreadsheet")
+    write_spreadsheet(dfs, args.outfile)
+
+    logger.info("Finished.")
+
+if __name__ == "__main__": main()
